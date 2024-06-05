152df221da1f497d23b852457d028f70ed007b48
Rodrigo Felipe Betussi <159579149+betussi@users.noreply.github.com>
2024-06-05 11:46:40 -0300

    Add files via upload

---
+++ DHT11_Sensor_MQTT.ino
@@ -0,0 +1,120 @@
+#include "DHT.h"
+#include <WiFi.h>
+extern "C" {
+  #include "freertos/FreeRTOS.h"
+  #include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+
+#define WIFI_SSID "Betussi"
+#define WIFI_PASSWORD "P@ssword1"
+#define MQTT_HOST IPAddress(192, 168, 17, 60)
+#define MQTT_PORT 1883
+
+#define MQTT_PUB_TEMP "esp32/dht/temperature"
+#define MQTT_PUB_HUM  "esp32/dht/humidity"
+#define MQTT_SUB_LED  "esp32/led/control" // Tópico para controlar o LED
+
+#define DHTPIN 4  
+#define DHTTYPE DHT11
+#define LED_BUILTIN 2 // Pino do LED interno no ESP32
+
+DHT dht(DHTPIN, DHTTYPE);
+float temp;
+float hum;
+
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
+unsigned long previousMillis = 0;
+const long interval = 10000;
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch(event) {
+    case IP_EVENT_STA_GOT_IP:
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      connectToMqtt();
+      break;
+    case WIFI_EVENT_STA_DISCONNECTED:
+      Serial.println("WiFi lost connection");
+      xTimerStop(mqttReconnectTimer, 0);
+      xTimerStart(wifiReconnectTimer, 0);
+      break;
+  }
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  mqttClient.subscribe(MQTT_SUB_LED, 1); // Inscreve-se no tópico do LED
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  String message = String(payload).substring(0, len);
+  if (String(topic) == MQTT_SUB_LED) {
+    if (message.equalsIgnoreCase("ON")) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println("LED turned ON");
+    } else if (message.equalsIgnoreCase("OFF")) {
+      digitalWrite(LED_BUILTIN, LOW);
+      Serial.println("LED turned OFF");
+    }
+  }
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  dht.begin();
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+  WiFi.onEvent(WiFiEvent);
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  connectToWifi();
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    hum = dht.readHumidity();
+    temp = dht.readTemperature();
+    if (isnan(temp) && isnan(hum)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
+      
+    // Publish an MQTT message on topic esp32/dht/temperature
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str());                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEMP, packetIdPub1);
+    Serial.printf("Message: %.2f \n", temp);
+
+    // Publish an MQTT message on topic esp32/dht/humidity
+    uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum).c_str());                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
+    Serial.printf("Message: %.2f \n", hum);
+    
+  }
+}
